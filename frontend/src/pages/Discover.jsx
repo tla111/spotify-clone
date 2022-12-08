4b619efaac74d6389df5d6d7b361a099d390e558
Tim La <tlamarketing111@gmail.com>
2022-12-07 19:03:17 -0500

    building layout for discover page

--- frontend/src/pages/Discover.jsx
+++ frontend/src/pages/Discover.jsx
@@ -1,3 +1,21 @@
-const Discover = () => <div>Discover</div>;
+import React from 'react';
+// import { useDispatch, useSelector } from 'react-redux';
+
+import { Error, Loader, SongCard } from '../components';
+// import { selectGenreListId } from '../redux/features/playerSlice';
+// import { useGetSongsByGenreQuery } from '../redux/services/shazamCore';
+import { genres } from '../assets/constants';
+
+const Discover = () => {
+  console.log(genres);
+  return (
+    <div className="flex flex-col">
+      <div className="w-full flex justify-between items-center sm:flex-row flex-col mt-4 mb-10">
+        {/* <select></select> */}
+      </div>
+
+    </div>
+  );
+};
 
 export default Discover;
