5d8631cbf62778d83f04fa59f7d9f838984700bb
Tim La <tlamarketing111@gmail.com>
2022-12-08 07:57:06 -0500

    set up handlePause & handleClick functions

--- frontend/src/components/SongCard.jsx
+++ frontend/src/components/SongCard.jsx
@@ -7,11 +7,23 @@ import { playPause, setActiveSong } from '../redux/features/playerSlice';
 
 const SongCard = ({ song, isPlaying, data, i }) => {
   const activeSong = 'Test';
+
+  const handlePauseClick = () => {
+
+  };
+
+  const handlePlayClick = () => {
+
+  };
   return (
     <div className="flex flex-col w-[250px] p-4 bg-white/5 bg-opacity-80 backdrop-blur-sm animate-slideup rounded-lg cursor-pointer">
       <div className="relative w-full h-56 group">
         <div className={`absolute inset-0 justify-center items-center bg-black bg-opacity-50 group-hover:flex ${activeSong?.title === song.title ? 'flex bg-black bg-opacity-70' : 'hidden'}`}>
-          <PlayPause />
+          <PlayPause
+            song={song}
+            handlePause={handlePauseClick}
+            handlePlay={handlePlayClick}
+          />
         </div>
         <img alt="song_img" src={song.images?.coverart} className="w-full h-full rounded-lg" />
       </div>
