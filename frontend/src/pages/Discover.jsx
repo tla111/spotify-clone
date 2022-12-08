efe5c1d9fd0bb2b978dceecb300023a8aa027d71
Tim La <tlamarketing111@gmail.com>
2022-12-07 19:28:03 -0500

    now mapping through real data

--- frontend/src/pages/Discover.jsx
+++ frontend/src/pages/Discover.jsx
@@ -9,7 +9,9 @@ import { genres } from '../assets/constants';
 const Discover = () => {
   const { data, isFetching, error } = useGetTopChartsQuery();
 
-  console.log(data);
+  if (isFetching) return <Loader title="Loading songs..." />;
+
+  if (error) return <Error />;
 
   const genreTitle = 'Pop';
   return (
@@ -26,7 +28,7 @@ const Discover = () => {
       </div>
 
       <div className="flex flex-wrap sm:justify-start justify-center gap-8">
-        {[1, 2, 3, 4, 5, 6, 7, 8, 9, 10].map((song, i) => (
+        {data?.map((song, i) => (
           <SongCard
             key={song.key}
             song={song}
