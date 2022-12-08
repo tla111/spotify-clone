f8753a6c756e7bce34de64644eedec37d240e690
Tim La <tlamarketing111@gmail.com>
2022-12-08 07:55:06 -0500

    song title & artist showing on card

--- frontend/src/components/SongCard.jsx
+++ frontend/src/components/SongCard.jsx
@@ -15,6 +15,19 @@ const SongCard = ({ song, isPlaying, data, i }) => {
         </div>
         <img alt="song_img" src={song.images?.coverart} className="w-full h-full rounded-lg" />
       </div>
+
+      <div className="mt-4 flex flex-col">
+        <p className="font-semibold text-lg text-white truncate">
+          <Link to={`/songs/${song?.key}`}>
+            {song.title}
+          </Link>
+        </p>
+        <p className="text-sm truncate text-gray-300 mt-1">
+          <Link to={song.artists ? `/artists/${song?.artists[0]?.adamid}` : '/top-artists'}>
+            {song.subtitle}
+          </Link>
+        </p>
+      </div>
     </div>
   );
 };
