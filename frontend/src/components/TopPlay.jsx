fc4bab73c98f6ea939839d7207c5b9223a9e18b4
Tim La <tlamarketing111@gmail.com>
2022-12-09 07:56:58 -0500

    building layout for topCharts

--- frontend/src/components/TopPlay.jsx
+++ frontend/src/components/TopPlay.jsx
@@ -34,8 +34,16 @@ const TopPlay = () => {
   };
 
   return (
-    <div></div>
-  )
-
-}
+    <div ref={divRef} className="xl:ml-6 ml-0 xl:mb-0 mb-6 flex-1 xl:max-w-[500px] max-w-full flex flex-col">
+      <div className="w-full flex flex-col">
+        <div className="flex flex-row justify-between items-center">
+          <h2 className="text-white font-bold text-2xl">Top Charts</h2>
+          <Link to="/top-charts">
+            <p className="text-gray-300 text-base cursor-pointer">See more</p>
+          </Link>
+        </div>
+      </div>
+    </div>
+  );
+};
 export default TopPlay;
