65936956f081a3ef1ff516eb50ff16292691d080
Tim La <tlamarketing111@gmail.com>
2022-12-09 07:43:38 -0500

    imported imports for sidebar

--- frontend/src/components/Sidebar.jsx
+++ frontend/src/components/Sidebar.jsx
@@ -1,3 +1,10 @@
+import React, { useState } from 'react';
+import { NavLink } from 'react-router-dom';
+import { HiOutlineHashtag, HiOutlineHome, HiOutlineMenu, HiOutlinePhotograph, HiOutlineUserGroup } from 'react-icons/hi';
+import { RiCloseLine } from 'react-icons/ri';
+
+import { logo } from '../assets';
+
 const Sidebar = () => (
   <div>Sidebar</div>
 );
