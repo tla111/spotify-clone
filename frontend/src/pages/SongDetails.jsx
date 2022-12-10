02534dfd186523857eba09648420714a00193e3a
Tim La <tlamarketing111@gmail.com>
2022-12-10 06:26:30 -0500

    imported imports for songDetails

--- frontend/src/pages/SongDetails.jsx
+++ frontend/src/pages/SongDetails.jsx
@@ -1,3 +1,11 @@
+import React from 'react';
+import { useParams } from 'react-router-dom';
+import { useSelector, useDispatch } from 'react-redux';
+import { DetailsHeader, Error, Loader, RelatedSongs } from '../components';
+
+import { setActiveSong, playPause } from '../redux/features/playerSlice';
+import { useGetSongDetailsQuery, useGetSongRelatedQuery } from '../redux/services/shazamCore';
+
 const SongDetails = () => <div>SongDetails</div>;
 
 export default SongDetails;
