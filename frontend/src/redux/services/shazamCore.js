130ef52d508b35c44643e837f1169b01de352337
Tim La <tlamarketing111@gmail.com>
2022-12-10 07:32:52 -0500

    fetching getCountry data

--- frontend/src/redux/services/shazamCore.js
+++ frontend/src/redux/services/shazamCore.js
@@ -13,7 +13,7 @@ export const shazamCoreApi = createApi({
   endpoints: (builder) => ({
     getTopCharts: builder.query({ query: () => '/charts/world' }),
     // getSongsByGenre: builder.query({ query: (genre) => `/charts/genre-world?genre_code=${genre}` }),
-    // getSongsByCountry: builder.query({ query: (countryCode) => `/charts/country?country_code=${countryCode}` }),
+    getSongsByCountry: builder.query({ query: (countryCode) => `/charts/country?country_code=${countryCode}` }),
     // getSongsBySearch: builder.query({ query: (searchTerm) => `/search/multi?search_type=SONGS_ARTISTS&query=${searchTerm}` }),
     getArtistDetails: builder.query({ query: (artistId) => `/artists/details?artist_id=${artistId}` }),
     getSongDetails: builder.query({ query: ({ songid }) => `/tracks/details?track_id=${songid}` }),
@@ -24,7 +24,7 @@ export const shazamCoreApi = createApi({
 export const {
   useGetTopChartsQuery,
   // useGetSongsByGenreQuery,
-  // useGetSongsByCountryQuery,
+  useGetSongsByCountryQuery,
   // useGetSongsBySearchQuery,
   useGetArtistDetailsQuery,
   useGetSongDetailsQuery,
