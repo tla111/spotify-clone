91e5086c9c9fdd6ca9f8651b66b30d8c39eb7bc1
Tim La <tlamarketing111@gmail.com>
2022-12-10 06:30:47 -0500

    fetching song details

--- frontend/src/redux/services/shazamCore.js
+++ frontend/src/redux/services/shazamCore.js
@@ -16,7 +16,7 @@ export const shazamCoreApi = createApi({
     // getSongsByCountry: builder.query({ query: (countryCode) => `/charts/country?country_code=${countryCode}` }),
     // getSongsBySearch: builder.query({ query: (searchTerm) => `/search/multi?search_type=SONGS_ARTISTS&query=${searchTerm}` }),
     // getArtistDetails: builder.query({ query: (artistId) => `/artists/details?artist_id=${artistId}` }),
-    // getSongDetails: builder.query({ query: ({ songid }) => `/tracks/details?track_id=${songid}` }),
+    getSongDetails: builder.query({ query: ({ songid }) => `/tracks/details?track_id=${songid}` }),
     // getSongRelated: builder.query({ query: ({ songid }) => `/tracks/related?track_id=${songid}` }),
   }),
 });
