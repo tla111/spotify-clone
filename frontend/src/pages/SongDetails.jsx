1e00ba55e93884dfe93ef549a4f4bfb100e04a9d
Tim La <tlamarketing111@gmail.com>
2022-12-10 06:29:20 -0500

    building structure for songDetails

--- frontend/src/pages/SongDetails.jsx
+++ frontend/src/pages/SongDetails.jsx
@@ -4,8 +4,21 @@ import { useSelector, useDispatch } from 'react-redux';
 import { DetailsHeader, Error, Loader, RelatedSongs } from '../components';
 
 import { setActiveSong, playPause } from '../redux/features/playerSlice';
-import { useGetSongDetailsQuery, useGetSongRelatedQuery } from '../redux/services/shazamCore';
+// import { useGetSongDetailsQuery, useGetSongRelatedQuery } from '../redux/services/shazamCore';
 
-const SongDetails = () => <div>SongDetails</div>;
+const SongDetails = () => {
+    const dispatch = useDispatch();
+    const { songid, id: artistId } = useParams();
+    const { activeSong, isPlaying } = useSelector((state) => state.player);
+
+  return (
+    <div className="flex flex-col">
+       <DetailsHeader
+        artistId={artistId}
+        songData={songData}
+      />
+    </div>
+  );
+};
 
 export default SongDetails;
