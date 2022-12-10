0a470e829a3f01cc45ac907e9eabaf9071652552
Tim La <tlamarketing111@gmail.com>
2022-12-10 06:46:40 -0500

    building layout for RelatedSongs

--- frontend/src/components/RelatedSongs.jsx
+++ frontend/src/components/RelatedSongs.jsx
@@ -3,7 +3,9 @@ import React from 'react';
 import SongBar from './SongBar';
 
 const RelatedSongs = ({ data, artistId, isPlaying, activeSong, handlePauseClick, handlePlayClick }) => (
-  <div>Loader</div>
+  <div className="flex flex-col">
+    <h1 className="font-bold text-3xl text-white">Related Songs:</h1>
+  </div>
 );
 
 export default RelatedSongs;
