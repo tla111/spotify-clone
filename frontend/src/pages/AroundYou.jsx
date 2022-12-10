17df214cc923c5ca5644744468e8de4fad22156a
Tim La <tlamarketing111@gmail.com>
2022-12-10 07:33:38 -0500

    implemented loader & error

--- frontend/src/pages/AroundYou.jsx
+++ frontend/src/pages/AroundYou.jsx
@@ -3,13 +3,13 @@ import axios from 'axios';
 import { useSelector } from 'react-redux';
 
 import { Error, Loader, SongCard } from '../components';
-// import { useGetSongsByCountryQuery } from '../redux/services/shazamCore';
+import { useGetSongsByCountryQuery } from '../redux/services/shazamCore';
 
 const CountryTracks = () => {
   const [country, setCountry] = useState('');
   const [loading, setLoading] = useState(true);
   const { activeSong, isPlaying } = useSelector((state) => state.player);
-  //   const { data, isFetching, error } = useGetSongsByCountryQuery(country);
+  const { data, isFetching, error } = useGetSongsByCountryQuery(country);
 
   useEffect(() => {
     axios
@@ -19,7 +19,10 @@ const CountryTracks = () => {
       .finally(() => setLoading(false));
   }, [country]);
 
-  console.log(country);
+  if (isFetching && loading) return <Loader title="Loading Songs around you..." />;
+
+  if (error && country !== '') return <Error />;
+
   return (
     <div>
       hi
