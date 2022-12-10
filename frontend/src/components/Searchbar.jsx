7422bf1ced8b585a5d13ed30dff087f466a9eae1
Tim La <tlamarketing111@gmail.com>
2022-12-10 07:41:05 -0500

    implemented imports for SearchBar

--- frontend/src/components/Searchbar.jsx
+++ frontend/src/components/Searchbar.jsx
@@ -1,3 +1,8 @@
+import React, { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+
+import { FiSearch } from 'react-icons/fi';
+
 const Searchbar = () => (
   <form onSubmit={handleSubmit} autoComplete="off" className="p-2 text-gray-400 focus-within:text-gray-600">
     <label htmlFor="search-field" className="sr-only">
