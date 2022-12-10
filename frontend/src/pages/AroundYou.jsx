d6472269ab3500f87c6d471974cf51d6dc1f7b02
Tim La <tlamarketing111@gmail.com>
2022-12-10 07:31:20 -0500

    geo location working

--- frontend/src/pages/AroundYou.jsx
+++ frontend/src/pages/AroundYou.jsx
@@ -3,14 +3,23 @@ import axios from 'axios';
 import { useSelector } from 'react-redux';
 
 import { Error, Loader, SongCard } from '../components';
-import { useGetSongsByCountryQuery } from '../redux/services/shazamCore';
+// import { useGetSongsByCountryQuery } from '../redux/services/shazamCore';
 
 const CountryTracks = () => {
   const [country, setCountry] = useState('');
   const [loading, setLoading] = useState(true);
   const { activeSong, isPlaying } = useSelector((state) => state.player);
-  const { data, isFetching, error } = useGetSongsByCountryQuery(country);
+  //   const { data, isFetching, error } = useGetSongsByCountryQuery(country);
 
+  useEffect(() => {
+    axios
+      .get('https://geo.ipify.org/api/v2/country?apiKey=at_5QNAeKw7tlfQMSsDRbSfau8J8CWXP')
+      .then((res) => setCountry(res?.data?.location.country))
+      .catch((err) => console.log(err))
+      .finally(() => setLoading(false));
+  }, [country]);
+
+  console.log(country);
   return (
     <div>
       hi
